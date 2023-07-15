692f155c265bbd896b5878da5c2a07df83871eb5
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-15 18:08:22 +0530

    implement routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.styles.scss';
+
+import RootLayout from './pages/Root';
+import HomePage from './pages/Home/Home';
+import TaskPage from './pages/Tasks/Tasks';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/tasks', element: <TaskPage /> }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
