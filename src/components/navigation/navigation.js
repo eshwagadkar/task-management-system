05aaedf1eed1585017ff48320f16428259edb854
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-15 17:48:09 +0530

    navigation component

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <header>
+            <nav className='navigation'>
+                 {/* Logo Container */}
+                 {/* <Link to="/" className='logo-container'>
+                    <Logo className='logo'/>
+                 </Link> */}
+
+                {/* Navbar links */}
+                <ul className='nav-links-container'>
+                    <li className='nav-link'><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation;
