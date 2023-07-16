186d3571abc3bddcbaeb4d557151bc629244044b
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-16 14:23:43 +0530

    bacdrop using hook

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -1,23 +1,43 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import BackDrop from '../../utility/Components/UIElements/BackDrop'; 
 
 import './MainNavigation.styles.scss';
 
 const MainNavigation = props => {
+
+    const [ drawerIsOpen, setDrawerIsOpen ] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
     return <>
+        { drawerIsOpen && <BackDrop onClick={closeDrawer}/>}
         {/* Side Drawer */}
-        <SideDrawer>
-            <nav className="main-navigation__drawer-nav">
-                <NavLinks />
-            </nav>
-        </SideDrawer>
+        {
+            drawerIsOpen ? (
+                <SideDrawer>
+                    <nav className="main-navigation__drawer-nav">
+                      <NavLinks />
+                    </nav>
+                </SideDrawer>
+            ) : null
+        }
+        
 
         {/* Main Page Header */}
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+
+            <button className="main-navigation__menu-btn" onClick={openDrawer} >
                 <span />
                 <span />
                 <span />
