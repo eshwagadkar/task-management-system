6f4ff7ae08f164430d7e17b3c76a295bb1ccaacf
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-17 20:43:34 +0530

    button component modifications

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom';
 
-import './Button.styles.scss';
+import './Button.css';
 
 const Button = props => {
 
   if (props.href) {
     return (
       <a
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${props.inverse && 'button--inverse'} ${props.danger && 'button--danger'}`}
         href={props.href}
       >
         {props.children}
@@ -21,8 +20,7 @@ const Button = props => {
       <Link
         to={props.to}
         exact={props.exact}
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${props.inverse && 'button--inverse'} ${props.danger && 'button--danger'}`}
       >
         {props.children}
       </Link>
@@ -31,8 +29,7 @@ const Button = props => {
   
   return (
     <button
-      className={`button button--${props.size || 'default'} ${props.inverse &&
-        'button--inverse'} ${props.danger && 'button--danger'}`}
+      className={`button button--${props.size || 'default'} ${props.inverse && 'button--inverse'} ${props.danger && 'button--danger'}`}
       type={props.type}
       onClick={props.onClick}
       disabled={props.disabled}
