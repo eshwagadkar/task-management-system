cfd9acd7e59144b655872481cc156b191fb5bb37
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-17 15:09:50 +0530

    Button component inside TaskItem component

--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -1,27 +1,34 @@
-import Card from '../../utility/Components/UIElements/Card';
+import { useState } from 'react';
+
+import Card from '../../utility/components/UIElements/Card';
+import Button from '../../utility/components/FormElements/Button';
 
 import './TaskItem.styles.scss';
 
 const TaskItem = props => {
-    
+
+    // const [] = useState(false);
+
     const { title, description, dueDate } = props.task;
     return <>
-        <li className='task-item'>
-            <Card className='task-item__content'>
-                {/* Task Item Info */}
-                <div className='task-item__info'>
-                    <h2>{title}</h2>
-                    <h3>{description}</h3>
-                    <p>{dueDate}</p>
-                </div>
+        {/* <Modal > */}
+            <li className='task-item'>
+                <Card className='task-item__content'>
+                    {/* Task Item Info */}
+                    <div className='task-item__info'>
+                        <h2>{title}</h2>
+                        <h3>{description}</h3>
+                        <p>{dueDate}</p>
+                    </div>
 
-                {/* Task Actions */}
-                <div className='task-item__actions'>
-                    <button>EDIT</button>
-                    <button>DELETE</button>
-                </div>
-            </Card>
-        </li>
+                    {/* Task Actions */}
+                    <div className='task-item__actions'>
+                        <Button to={`/tasks/${props.id}`}>EDIT</Button>
+                        <Button danger>Delete</Button>
+                    </div>
+                </Card>
+            </li>
+        {/* </Modal> */}
     </>
 }
 
