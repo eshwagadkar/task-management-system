a3be7385699a55f96942b988a87357e55c6b2556
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-17 23:28:21 +0530

    register list tasks and update task

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.styles.scss';
 import RootLayout from './pages/Root';
 import HomePage from './pages/Home/HomePage';
 import NewTask from './pages/Tasks/NewTask';
-import UsersTaskListingPage from './pages/Tasks/UsersTaskListingPage';
+import UpdateTask from './pages/Tasks/UpdateTask';
+import ListUserTasks from './pages/Tasks/ListUserTasks';
 import UserPage from './pages/Users/Users';
 
 const router = createBrowserRouter([
@@ -13,8 +14,9 @@ const router = createBrowserRouter([
     element : <RootLayout />,
     children: [
       { path: '/', element: <HomePage /> },
+      { path: '/:userId/tasks', element: <ListUserTasks /> },
       { path: '/users/new', element: <NewTask /> },
-      { path: '/:userId/tasks', element: <UsersTaskListingPage /> },
+      { path: '/tasks/:taskid', element: <UpdateTask /> },
       { path: '/users', element: <UserPage /> }
     ]
   }
