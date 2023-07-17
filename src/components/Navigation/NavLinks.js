5c68b03aaea46e3a308733eaca31221e24f3866d
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-17 09:48:29 +0530

    navigation links

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 import './NavLinks.styles.scss';
 
 const NavLinks = props => {
-    return <ul className='nav-links'>
-            <li><Link to="/users/new">Tasks</Link></li>
-            <li><Link to="/users">Users</Link></li>
-         </ul>
+    return <>
+        <ul className='nav-links'>
+            <li><Link to="/users/new">Create Tasks</Link></li>
+            <li><Link to="/users/tasks">List the Tasks</Link></li>
+        </ul>
+    </>
 }
 
 export default NavLinks;
