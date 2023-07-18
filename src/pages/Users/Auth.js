72be936dffd5fce0e0aca9f67576b495919e39c4
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-18 16:49:29 +0530

    add signup and switch mode button

--- src/pages/Users/Auth.js
+++ src/pages/Users/Auth.js
@@ -6,7 +6,7 @@ import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/utility/validator';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/utility/validator';
 
 import './Auth.styles.scss';
 
@@ -15,7 +15,7 @@ const Auth = () => {
 
     const [ isLoginMode, setIsLoginMode ] = useState(true);
 
-    const [ formState, inputHandler ] = useForm({
+    const [ formState, inputHandler, setFormData ] = useForm({
         email: {
             value: '',
             isValid: false
@@ -28,6 +28,25 @@ const Auth = () => {
     false );
 
     const switchModeHandler = () => {
+
+        if(!isLoginMode){
+            setFormData(
+                {   
+                    ...formState.inputs,
+                    name: undefined
+                },
+                formState.inputs.email.isValid && formState.inputs.password.isValid
+            )
+        } else {
+            setFormData({
+                ...formState.inputs,
+                name:  {
+                    value: '',
+                    isValid: false
+                   }
+              }, false );
+        }
+
         setIsLoginMode(prevMode => !prevMode );
     }
 
@@ -42,6 +61,20 @@ const Auth = () => {
             <hr />
             <form onSubmit={authSubmitHandler}>
                 {/* Email Input */}
+
+                {
+                    !isLoginMode && (
+                        <Input
+                            id='name'
+                            element='input'
+                            type="text"
+                            label='Your Name'
+                            validators={ [VALIDATOR_REQUIRE()]}
+                            errorText="Please enter a name."
+                            onInput={inputHandler}
+                        />
+                    )
+                }
                 <Input
                  id='email'
                  element='input'
